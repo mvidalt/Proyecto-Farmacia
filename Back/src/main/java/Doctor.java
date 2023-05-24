c00ce6d583282d37e639bd0d254a89f265b39d5d
mvidalt <mvidalt@cifpfbmoll.eu>
2023-05-24 20:53:29 +0200

    feat: :sparkles: detectar si el inicio de sesión es correcto

--- Back/src/main/java/Doctor.java
+++ Back/src/main/java/Doctor.java
@@ -1,5 +1,10 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Doctor extends Person {
 
@@ -19,8 +24,36 @@ public class Doctor extends Person {
 	}
 
 	public void login(String mail, String pass) {
-		
+		String url = "jdbc:mysql://localhost:3306/farmacia";
+		String username = "root";
+		String password = "";
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+			// Verificar si el correo electrónico y la contraseña son correctos
+	        String query = "SELECT * FROM doctor WHERE mail = ? AND pass = ?";
+			PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, mail);
+            statement.setString(2, pass);
+			ResultSet resultSet = statement.executeQuery();
+
+			if (resultSet.next()) {
+				// El correo electrónico y la contraseña son correctos
+				// Cargar los atributos de la base de datos a través del método load()
+				//load(resultSet);
+
+				// Actualizar los atributos lastLog y session en la base de datos
+				//updateLastLogAndSession(connection);
+
+				System.out.println("Inicio de sesión exitoso.");
+			} else {
+				// El correo electrónico y/o la contraseña son incorrectos
+				System.out.println("Credenciales inválidas.");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
+
+	
 
 	public boolean isLogged(String mail, String session) {
 		// Comprueba si el mail y la session son correctos
@@ -41,10 +74,9 @@ public class Doctor extends Person {
 	}
 
 	public String getTable() {
-		// Devuelve una cadena de tabla HTML con 
-		//todas las fichas activas asociadas al médico        
+		// Devuelve una cadena de tabla HTML con
+		// todas las fichas activas asociadas al médico
 		return "";
 	}
-	
-}
 
+}
--- Back/src/main/java/Login.java
+++ Back/src/main/java/Login.java
@@ -33,41 +33,24 @@ public class Login extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Datos de conexión a la base de datos
-        String url = "jdbc:mysql://localhost:3306/farmacia";
+
+		String url = "jdbc:mysql://localhost:3306/farmacia";
         String username = "root";
         String password = "";
         
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		String email = request.getParameter("email");
 		String pass = request.getParameter("pass");
 		System.out.println("Usuario: "+email);
 		System.out.println("Pass: "+pass);
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		
 		ConnectionDB db = new ConnectionDB();
 		db.connectar();
 		db.close();
-		// Establecer la conexión
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            // Crear una declaración
-            Statement statement = connection.createStatement();
-            
-            // Ejecutar una consulta
-            String query = "SELECT * FROM doctor WHERE mail='"+email+"' AND pass='"+pass+"'";
-            ResultSet resultSet = statement.executeQuery(query);
-            
-            // Recorrer los resultados y mostrarlos en la consola
-            while (resultSet.next()) {
-                String mail = resultSet.getString("mail");
-                String name = resultSet.getString("name");
-                System.out.println("ID: " + mail + ", Nombre: " + name);
-            }
-            
-            // Cerrar el resultado y la declaración
-            resultSet.close();
-            statement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+		Doctor d1 = new Doctor();
+		d1.login(email, pass);
+		
+
+
     }
 		
 		
